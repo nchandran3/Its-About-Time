410eaf1afe7f802ff001b5a55b67216ed75a6817
Naveen Chandran <naveenkchandran@gmail.com>
2014-07-22 00:21:30 +0200

    Uploaded Animations.js to contain animation functions for any html objects

---
+++ js/Animations.js
@@ -0,0 +1,14 @@
+/**
+ * Javascript that animates the markers on the map when the website starts up
+ * @author Naveen Chandran
+ */
+
+function dropMarkers()
+{
+	var markers_arr = $(".marker").slice(0,6);
+	
+	for (marker: markers_arr)
+		{
+			
+		}
+}

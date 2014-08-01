361cb9e36e17cfdc2a4fa75a8ee248284463846f
Naveen Chandran <naveenkchandran@gmail.com>
2014-08-01 04:49:28 -0400

    Organized images and changed images.js paths

--- js/images.js
+++ js/images.js
@@ -18,42 +18,42 @@ var imagesJSON = '{\
         "times" : [\
             {\
                 "name": "Day",\
-                "thumb": "images/reialdaythumb.jpg",\
+                "thumb": "images/Louis/reialdaythumb.jpg",\
                 "images": [\
                     {\
-                        "url": "images/reialday2.jpg",\
+                        "url": "images/Louis/reialday2.jpg",\
                         "caption": "In the daytime and evening captures of the plaza, tourists sit to enjoy a meal. At night, tables and umbrellas are stacked, and tourists gather outside after clubs have closed for beers, cigarettes, and socializing. "\
                     },\
                     {\
-                        "url": "images/reialday1.jpg",\
+                        "url": "images/Louis/reialday1.jpg",\
                         "caption": "Pla&ccedil;a Reial, just off of La Rambla de Catalunya, is inhabited by tourists all 24 hours of the day. Notice how daytime occupants sit to admire the surroundings, and nighttime and evening inhabitants gather in clusters to for social interaction, taking up Estrella cans and cigarettes after dinner or clubbing. "\
                     }\
                 ]\
             },\
             {\
                 "name": "Evening",\
-                "thumb": "images/reialevethumb.jpg",\
+                "thumb": "images/Louis/reialevethumb.jpg",\
                 "images": [\
                     {\
-                        "url": "images/reialeve2.jpg",\
+                        "url": "images/Louis/reialeve2.jpg",\
                         "caption": "In the daytime and evening captures of the plaza, tourists sit to enjoy a meal. At night, tables and umbrellas are stacked, and tourists gather outside after clubs have closed for beers, cigarettes, and socializing. "\
                     },\
                     {\
-                        "url": "images/reialeve1.jpg",\
+                        "url": "images/Louis/reialeve1.jpg",\
                         "caption": "Pla&ccedil;a Reial, just off of La Rambla de Catalunya, is inhabited by tourists all 24 hours of the day. Notice how daytime occupants sit to admire the surroundings, and nighttime and evening inhabitants gather in clusters to for social interaction, taking up Estrella cans and cigarettes after dinner or clubbing. "\
                     }\
                 ]\
             },\
             {\
                 "name": "Night",\
-                "thumb": "images/reialnightthumb.jpg",\
+                "thumb": "images/Louis/reialnightthumb.jpg",\
                 "images": [\
                     {\
-                        "url": "images/reialnight2.jpg",\
+                        "url": "images/Louis/reialnight2.jpg",\
                         "caption": "In the daytime and evening captures of the plaza, tourists sit to enjoy a meal. At night, tables and umbrellas are stacked, and tourists gather outside after clubs have closed for beers, cigarettes, and socializing. "\
                     },\
                     {\
-                        "url": "images/reialnight1.jpg",\
+                        "url": "images/Louis/reialnight1.jpg",\
                         "caption": "Pla&ccedil;a Reial, just off of La Rambla de Catalunya, is inhabited by tourists all 24 hours of the day. Notice how daytime occupants sit to admire the surroundings, and nighttime and evening inhabitants gather in clusters to for social interaction, taking up Estrella cans and cigarettes after dinner or clubbing."\
                     }\
                 ]\
@@ -73,66 +73,66 @@ var imagesJSON = '{\
         "times" : [\
             {\
                 "name": "Day",\
-                "thumb": "images/macbadaythumb.jpg",\
+                "thumb": "images/Louis/macbadaythumb.jpg",\
                 "images" : [\
                     {\
-                        "url": "images/macbaday1.jpg",\
+                        "url": "images/Louis/macbaday1.jpg",\
                         "caption": "During the day, tourists pass by to visit the MACBA, and as the sun sets, the youth and skateboarders flock to the plaza, and locals walk their dogs. Skateboarders control this plaza until the dead of night, when at last only the cerveza salesmen and drunks remain."\
                     },\
                     {\
-                        "url": "images/macbaday2.jpg",\
+                        "url": "images/Louis/macbaday2.jpg",\
                         "caption": "Skateboarders occupy this plaza all 24 hours of the day, at their highest concentration as the sun sets. Locals come here to enjoy beer, drugs, and the social atmosphere. As compared to other parts of the city, photography is not questioned, but welcomed."\
                     },\
                     {\
-                        "url": "images/macbaday3.jpg",\
+                        "url": "images/Louis/macbaday3.jpg",\
                         "caption": "As skateboarders come to enjoy their evening, they leave the remnants of their activities: beer cans, cigarette butts, and plastic bags. The trash stagnates here with no humans to complain or be bothered, at least until the water trucks come to wash it away."\
                     },\
                     {\
-                        "url": "images/macbaday4.jpg",\
+                        "url": "images/Louis/macbaday4.jpg",\
                         "caption": "The relationship between the museum and the skateboarders appears to be detrimental, but in reality comes at no harm to the museum or its contents. The exterior is durable and expansive, and the local people have come only naturally to fill the void it has created. A museum of modern art benefits from the youthful and trendy surroundings."\
                     }\
                 ]\
             },\
             {\
                 "name": "Evening",\
-                "thumb": "images/macbaeveningthumb.jpg",\
+                "thumb": "images/Louis/macbaeveningthumb.jpg",\
                 "images" : [\
                     {\
-                        "url": "images/macbaeve1.jpg",\
+                        "url": "images/Louis/macbaeve1.jpg",\
                         "caption": "During the day, tourists pass by to visit the MACBA, and as the sun sets, the youth and skateboarders flock to the plaza, and locals walk their dogs. Skateboarders control this plaza until the dead of night, when at last only the cerveza salesmen and drunks remain. "\
                     },\
                     {\
-                        "url": "images/macbaeve2.jpg",\
+                        "url": "images/Louis/macbaeve2.jpg",\
                         "caption": "Skateboarders occupy this plaza all 24 hours of the day, at their highest concentration as the sun sets. Locals come here to enjoy beer, drugs, and the social atmosphere. As compared to other parts of the city, photography is not questioned, but welcomed."\
                     },\
                     {\
-                        "url": "images/macbaeve3.jpg",\
+                        "url": "images/Louis/macbaeve3.jpg",\
                         "caption": "As skateboarders come to enjoy their evening, they leave the remnants of their activities: beer cans, cigarette butts, and plastic bags. The trash stagnates here with no humans to complain or be bothered, at least until the water trucks come to wash it away."\
                     },\
                     {\
-                        "url": "images/macbaeve4.jpg",\
+                        "url": "images/Louis/macbaeve4.jpg",\
                         "caption": "The relationship between the museum and the skateboarders appears to be detrimental, but in reality comes at no harm to the museum or its contents. The exterior is durable and expansive, and the local people have come only naturally to fill the void it has created. A museum of modern art benefits from the youthful and trendy surroundings."\
                     }\
                 ]\
             },\
             {\
                 "name": "Night",\
-                "thumb": "images/macbanightthumb.jpg",\
+                "thumb": "images/Louis/macbanightthumb.jpg",\
                 "images": [\
                     {\
-                        "url": "images/macbanight1.jpg",\
+                        "url": "images/Louis/macbanight1.jpg",\
                         "caption": "During the day, tourists pass by to visit the MACBA, and as the sun sets, the youth and skateboarders flock to the plaza, and locals walk their dogs. Skateboarders control this plaza until the dead of night, when at last only the cerveza salesmen and drunks remain. "\
                     },\
                     {\
-                        "url": "images/macbanight2.jpg",\
+                        "url": "images/Louis/macbanight2.jpg",\
                         "caption": "Skateboarders occupy this plaza all 24 hours of the day, at their highest concentration as the suhn sets. Locals come here to enjoy beer, drugs, and the social atmosphere. As compared to other parts of the city, photography is not questioned, but welcomed."\
                     },\
                     {\
-                        "url": "images/macbanight3.jpg",\
+                        "url": "images/Louis/macbanight3.jpg",\
                         "caption": "As skateboarders come to enjoy their evening, they leave the remnants of their activities: beer cans, cigarette butts, and plastic bags. The trash stagnates here with no humans to complain or be bothered, at least until the water trucks come to wash it away."\
                     },\
                     {\
-                        "url": "images/macbanight4.jpg",\
+                        "url": "images/Louis/macbanight4.jpg",\
                         "caption": "The relationship between the museum and the skateboarders appears to be detrimenal, but in reality comes at no harm to the museum or its contents. The exterior is durable and expansive, and the local people have come only naturally to fill the void it has created. A museum of modern art benefits from the youthful and trendy surroundings."\
                     }\
                 ]\

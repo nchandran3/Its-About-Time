0ce2439e01daf67505db35fbcf36f7a928c78fc0
Naveen Chandran <naveenkchandran@gmail.com>
2014-08-01 20:28:23 -0400

    Added captions to images

--- js/images.js
+++ js/images.js
@@ -1,4 +1,9 @@
 var images = {
+		
+	/*************************
+	 * RAMBLA DE RAVAL
+	 *************************/
+		
 	"raval" : {
 		"name" : "Rambla de Raval",
 		"location" : [ 388, 228 ],
@@ -9,19 +14,19 @@ var images = {
 					"images" : [
 							{
 								"url" : "images/Naveen/RavalDay1.jpg",
-								"caption" : "During the day, tourists pass by to visit the MACBA, and as the sun sets, the youth and skateboarders flock to the plaza, and locals walk their dogs. Skateboarders control this plaza until the dead of night, when at last only the cerveza salesmen and drunks remain."
+								"caption" : "During the day, this ebullient neighborhood is packed with residents, foreigners, and everything in between. It surges with pedestrian traffic down its narrow, yet familiarly structured walkway. Stores are put in makeshift tents, giving a quick appeal to any wandering eyes, and displaying their goods in accordance with the hurried lifestyle. Trees are planted periodically to provide shade and relief to exhausted consumers – not only of the stores, but of the restaurants as well. "
 							},
 							{
 								"url" : "images/Naveen/RavalDay2.jpg",
-								"caption" : "Skateboarders occupy this plaza all 24 hours of the day, at their highest concentration as the sun sets. Locals come here to enjoy beer, drugs, and the social atmosphere. As compared to other parts of the city, photography is not questioned, but welcomed."
+								"caption" : "Viewing La Rambla de Raval from the other side, we see the threshold between consumerism and liberality. The preceding space is open and invites congregation not just by people, but feathery pheasants as well. The shade provided by the palm trees caters to a diverse group of weary citizens, one of which who is secure and trusting enough to take a nap on the cool ground. There are many dualities present here: man and beast, light and dark, and commercial vs natural. "
 							},
 							{
 								"url" : "images/Naveen/RavalDay3.jpg",
-								"caption" : "As skateboarders come to enjoy their evening, they leave the remnants of their activities: beer cans, cigarette butts, and plastic bags. The trash stagnates here with no humans to complain or be bothered, at least until the water trucks come to wash it away."
+								"caption" : "A close up of the shops shows a surprising cultural influence; the tents are lined with a non-native cloth and pattern, speaking to the heterogeneous makeup of the owners and residents. In a mix of modern and traditional, the tents juxtaposed with the cherry picker convey the intertwined duality of the area. "
 							},
 							{
 								"url" : "images/Naveen/RavalDay4.jpg",
-								"caption" : "The relationship between the museum and the skateboarders appears to be detrimental, but in reality comes at no harm to the museum or its contents. The exterior is durable and expansive, and the local people have come only naturally to fill the void it has created. A museum of modern art benefits from the youthful and trendy surroundings."
+								"caption" : "This shop proudly presents shirts that boast the name of the neighborhood. It shows their deep appreciation of their surroundings and pride in their complex, not to mention their sick graphic design skills."
 							} ]
 				},
 				{
@@ -30,15 +35,15 @@ var images = {
 					"images" : [
 							{
 								"url" : "images/Naveen/RavalEve1.jpg",
-								"caption" : "During the day, tourists pass by to visit the MACBA, and as the sun sets, the youth and skateboarders flock to the plaza, and locals walk their dogs. Skateboarders control this plaza until the dead of night, when at last only the cerveza salesmen and drunks remain. "
+								"caption" : "As dusk looms in Raval, the streets are flooded with a diverse sea of people. There is young and old, and many different people of differing origins. The benches are packed with the exhausted as many residents make their way back for a quick meal or to freshen up for the night. The occasional tourist is also spotted, but obscured by the locals. "
 							},
 							{
 								"url" : "images/Naveen/RavalEve2.jpg",
-								"caption" : "Skateboarders occupy this plaza all 24 hours of the day, at their highest concentration as the sun sets. Locals come here to enjoy beer, drugs, and the social atmosphere. As compared to other parts of the city, photography is not questioned, but welcomed."
+								"caption" : "A close up of the pedestrian traffic. What’s interesting to note is the aura of the people, especially with respect to their diversity, style, and socialization. There are various races, complexions, and genders all captured in this one image. Also depicted is the fashion of Barcelona – jean shorts and a tank or neutral t-shirt for men. Finally, save the rightmost group, there is no use of technology – people are actively conversing without using it as a crutch. "
 							},
 							{
 								"url" : "images/Naveen/RavalEve3.jpg",
-								"caption" : "As skateboarders come to enjoy their evening, they leave the remnants of their activities: beer cans, cigarette butts, and plastic bags. The trash stagnates here with no humans to complain or be bothered, at least until the water trucks come to wash it away."
+								"caption" : "In the evening, many people sit down to eat under the shade provided by the surrounding fauna. The restaurants are at a busy hour, as all along the street tables are filled with hungry customers. It is interesting to note that these tables are placed across the street on the middle walkway, far from the actual restaurant; they serve as an example of the appeal and utility of the walkway, as well as the lucrative planning of the restaurant owners. "
 							} ]
 				},
 				{
@@ -47,18 +52,26 @@ var images = {
 					"images" : [
 							{
 								"url" : "images/Naveen/RavalNight1.jpg",
-								"caption" : "During the day, tourists pass by to visit the MACBA, and as the sun sets, the youth and skateboarders flock to the plaza, and locals walk their dogs. Skateboarders control this plaza until the dead of night, when at last only the cerveza salesmen and drunks remain. "
+								"caption" : "The dynamic at night here is much like Arc de Triomf. Again, the highly contagious drinking culture is exposed to bring people together. The tents are entirely gone, and the space is at once dominated by locals who can even walk barefoot to the walkway to enjoy a beer. Everything is vacated save for the omnipresent cerveza men and their inebriated clienteles."
 							},
 							{
 								"url" : "images/Naveen/RavalNight2.jpg",
-								"caption" : "Skateboarders occupy this plaza all 24 hours of the day, at their highest concentration as the suhn sets. Locals come here to enjoy beer, drugs, and the social atmosphere. As compared to other parts of the city, photography is not questioned, but welcomed."
+								"caption" : "The neighborhood is very inviting – even though dominated by locals, everyone is welcome to contribute to the conversation (even Louis and I). The groups tend to position themselves along the outskirts to create their own private sub communities. "
 							},
 							{
 								"url" : "images/Naveen/RavalNight3.jpg",
-								"caption" : "As skateboarders come to enjoy their evening, they leave the remnants of their activities: beer cans, cigarette butts, and plastic bags. The trash stagnates here with no humans to complain or be bothered, at least until the water trucks come to wash it away."
+								"caption" : "This shot was taken a bit later, after the locals themselves had gone to bed to prepare for the next day. The cleaning truck has just passed by, whisking away any remnants of the night and preparing the ground for the deployment of tents and the constant scuffling of the day walkers. In a moment, the grounds so alive with cultures, stories, and community is turned into a desolate and barren environment, following the cycle of birth, death, and rebirth that occurs every day. "
 							} ]
 				} ]
 	},
+	
+	
+	
+	
+	/*********************
+	 * FORT PIENC
+	 *********************/
+	
 	"fortpienc" : {
 		"name" : "Fort Pienc",
 		"location" : [ 159, 1092 ],
@@ -69,11 +82,11 @@ var images = {
 					"images" : [
 							{
 								"url" : "images/Naveen/PiencDay1.jpg",
-								"caption" : "During the day, tourists pass by to visit the Pienc, and as the sun sets, the youth and skateboarders flock to the plaza, and locals walk their dogs. Skateboarders control this plaza until the dead of night, when at last only the cerveza salesmen and drunks remain."
+								"caption" : "During the day, Fort Pienc is little more than a space lush with foliage and benches that are predominantly empty. There is a market place and a school that compose the complex, which quite close to major walkways."
 							},
 							{
 								"url" : "images/Naveen/PiencDay2.jpg",
-								"caption" : "Skateboarders occupy this plaza all 24 hours of the day, at their highest concentration as the sun sets. Locals come here to enjoy beer, drugs, and the social atmosphere. As compared to other parts of the city, photography is not questioned, but welcomed."
+								"caption" : "Another angle of the space in the day time. The trees are aptly placed to provide shade to the resting, and there is a water fountain, illustrating the more sedentary practicality of the plaza. In a city where people are constantly bouncing from place to place, it makes sense that during the day there is little to no activity here. "
 							} ]
 				},
 				{
@@ -82,11 +95,11 @@ var images = {
 					"images" : [
 							{
 								"url" : "images/Naveen/PiencEve1.jpg",
-								"caption" : "During the day, tourists pass by to visit the Pienc, and as the sun sets, the youth and skateboarders flock to the plaza, and locals walk their dogs. Skateboarders control this plaza until the dead of night, when at last only the cerveza salesmen and drunks remain. "
+								"caption" : "This picture was taken during the evening in late May. The benches provide a perfect view to the dancers, and there are booths installed behind them. There are children in the background as well; they normally populate the area, playing soccer, riding bikes, or clinging to parents who have come to pick them up from school. "
 							},
 							{
 								"url" : "images/Naveen/PiencEve2.jpg",
-								"caption" : "Skateboarders occupy this plaza all 24 hours of the day, at their highest concentration as the sun sets. Locals come here to enjoy beer, drugs, and the social atmosphere. As compared to other parts of the city, photography is not questioned, but welcomed."
+								"caption" : ""
 							} ]
 				},
 				{
@@ -95,18 +108,24 @@ var images = {
 					"images" : [
 							{
 								"url" : "images/Naveen/PiencNight1.jpg",
-								"caption" : "During the day, tourists pass by to visit the Pienc, and as the sun sets, the youth and skateboarders flock to the plaza, and locals walk their dogs. Skateboarders control this plaza until the dead of night, when at last only the cerveza salesmen and drunks remain. "
+								"caption" : "At night, Fort Pienc becomes just as empty as it was in the day. There are no people here, except for the few homeless men and women who use the benches as beds and the canopy of the market as shelter from the elements. "
 							},
 							{
 								"url" : "images/Naveen/PiencNight2.jpg",
-								"caption" : "Skateboarders occupy this plaza all 24 hours of the day, at their highest concentration as the suhn sets. Locals come here to enjoy beer, drugs, and the social atmosphere. As compared to other parts of the city, photography is not questioned, but welcomed."
+								"caption" : "Another view showing the evaporating leftovers of the nightly cleaning that occurred soon before. The lights are hidden behind the trees, imitating the natural moonlight that shines down on clear nights. "
 							} ]
 				} ]
 	},
 
-	/**
+	
+	
+	
+	
+	
+	/*************************
 	 * PLACA REIAL
-	 */
+	 *************************/
+	
 	"placareial" : {
 		"name" : "Pla&ccedil;a Reial",
 		"location" : [ 487, 418 ],
@@ -151,6 +170,16 @@ var images = {
 							} ]
 				} ]
 	},
+	
+	
+	
+	
+	
+	/*****************************
+	 * ARC DE TRIOMF
+	 *****************************/
+	
+	
 	"arc" : {
 		"name" : "Arc de Triomf",
 		"location" : [ 287, 900 ],
@@ -161,15 +190,15 @@ var images = {
 					"images" : [
 							{
 								"url" : "images/Naveen/ArcDay1.jpg",
-								"caption" : "During the day, tourists pass by to visit the Arc, and as the sun sets, the youth and skateboarders flock to the plaza, and locals walk their dogs. Skateboarders control this plaza until the dead of night, when at last only the cerveza salesmen and drunks remain."
+								"caption" : "This picture, taken around 4 PM, shows how the Arc itself is used as a covering for a stage. Its form is naturally shaped like a covering, and it makes a great backdrop for any event. Fences are seen, because the predicted audience is quite large – a problem overcome but also emphasized by the wide bounds and length of the walkway. The people are mainly tourists, identified by their backpacks and outlandish style."
 							},
 							{
 								"url" : "images/Naveen/ArcDay2.jpg",
-								"caption" : "Skateboarders occupy this plaza all 24 hours of the day, at their highest concentration as the sun sets. Locals come here to enjoy beer, drugs, and the social atmosphere. As compared to other parts of the city, photography is not questioned, but welcomed."
+								"caption" : "This picture shows another perspective of the Arc, the long walkway. The types of people shown are mainly tourists, walking to their next landmark, or coming back to their hostels. It is important to note how the sides of the walkway are not populated – it is a time for commuting, not settling."
 							},
 							{
 								"url" : "images/Naveen/ArcDay3.jpg",
-								"caption" : "As skateboarders come to enjoy their evening, they leave the remnants of their activities: beer cans, cigarette butts, and plastic bags. The trash stagnates here with no humans to complain or be bothered, at least until the water trucks come to wash it away."
+								"caption" : "Further down the walkway, street performers and vendors showcase their various wares, attracting children and inviting people to take a short break from their bustling day. Also pictured is a bike, which is an essential possession for all Barcelonians and a representation of the highly mobile lifestyle. Although many people are still focused on their travel, the distinct culture begins to manifest itself, giving the perceptive tourist a taste of what is to come later on in the night. "
 							} ]
 				},
 				{
@@ -178,15 +207,15 @@ var images = {
 					"images" : [
 							{
 								"url" : "images/Naveen/ArcEve1.jpg",
-								"caption" : "During the day, tourists pass by to visit the Arc, and as the sun sets, the youth and skateboarders flock to the plaza, and locals walk their dogs. Skateboarders control this plaza until the dead of night, when at last only the cerveza salesmen and drunks remain. "
+								"caption" : "In the evening, there is a great vicissitude in the once commercialized space underneath the Arc. The stage has been cleared; the teenagers and locals begin to congregate to practice their various passions: skateboarding, rollerblading, dancing, singing, and myriad others. Although free from attractions or organized gathering, the Arc is seen to be just as lively by the numerous groups that come to hang out and hone their skills. The space is utilized according to what fits the needs of the inhabitants – it is molded by necessity. "
 							},
 							{
 								"url" : "images/Naveen/ArcEve2.jpg",
-								"caption" : "Skateboarders occupy this plaza all 24 hours of the day, at their highest concentration as the sun sets. Locals come here to enjoy beer, drugs, and the social atmosphere. As compared to other parts of the city, photography is not questioned, but welcomed."
+								"caption" : "This image shows the other end during the evening. A group is dancing, using the benches as spectator stands and awaiting their turns to impress one another. They utilize the open spaces and exercise their independence to express themselves. "
 							},
 							{
 								"url" : "images/Naveen/ArcEve3.jpg",
-								"caption" : "As skateboarders come to enjoy their evening, they leave the remnants of their activities: beer cans, cigarette butts, and plastic bags. The trash stagnates here with no humans to complain or be bothered, at least until the water trucks come to wash it away."
+								"caption" : "This picture shows the diversity present in the evening. A group of Korean women practice their traditional dance, even stowing along their young child for the occasion. They practice beneath a bright light in order to coordinate their movements."
 							} ]
 				},
 				{
@@ -195,18 +224,22 @@ var images = {
 					"images" : [
 							{
 								"url" : "images/Naveen/ArcNight1.jpg",
-								"caption" : "During the day, tourists pass by to visit the Arc, and as the sun sets, the youth and skateboarders flock to the plaza, and locals walk their dogs. Skateboarders control this plaza until the dead of night, when at last only the cerveza salesmen and drunks remain. "
+								"caption" : "Late during the night, the flood of locals dissipates, and the Arc is left with an empty environment; however, it is not lonely. A group of twenty-somethings continues to practice their dance, and two Pakistani men stick it out to sell their very last cerveza to the probable drunken wanderer. People use the stone slabs to stabilize their spinning heads, and to banter with friends before retiring to their homes."
 							},
 							{
 								"url" : "images/Naveen/ArcNight2.jpg",
-								"caption" : "Skateboarders occupy this plaza all 24 hours of the day, at their highest concentration as the suhn sets. Locals come here to enjoy beer, drugs, and the social atmosphere. As compared to other parts of the city, photography is not questioned, but welcomed."
-							},
-							{
-								"url" : "images/Naveen/ArcNight3.jpg",
-								"caption" : "As skateboarders come to enjoy their evening, they leave the remnants of their activities: beer cans, cigarette butts, and plastic bags. The trash stagnates here with no humans to complain or be bothered, at least until the water trucks come to wash it away."
+								"caption" : "The opposite side shows a couple walking home late at night, and a few lonely men on the benches. The lights overhang them as if to illuminate their solitude; but who knows if they are waiting on a straggling companion or just infusing themselves with the tranquility of the vacant area. Most likely, they will pass out here, amidst the Estrella cans that line their sitting space. "
 							} ]
 				} ]
 	},
+	
+	
+	
+	
+	/*****************************************
+	 * MACBA
+	 *****************************************/
+	
 	"macba" : {
 		"name" : "MACBA",
 		"location" : [ 201, 305 ],
